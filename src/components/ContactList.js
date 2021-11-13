22c21057fb9868b2f2271f815392aead67d80510
aditReza <lete.note3@gmail.com>
2021-11-14 04:44:57 +0700

    update contact form Container

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,21 +3,21 @@ import React from "react";
 const ContactList = (props) => {
   const { contactList = [] } = props;
   return contactList.length === 0
-    ? <>No Contact List</>
+    ? <div style={{ textAlign: 'center' }}>No Contact List</div>
     : contactList.map(( contact, contactIndex ) => {
-    return (
-      <div className="contact" key={`${contactIndex}`}>
-        <div className="contact-info">
-          <p className="contact-info-name">{contact.name}</p>
-          <p className="contact-info-number">{contact.number}</p>
-        </div>
-        <div className="contact-actions">
-          <button className="btn btn-primary">Edit</button>
-          <button className="btn btn-danger">Delete</button>
-        </div>
-      </div>
-    )
-  });
+        return (
+          <div className="contact" key={`${contactIndex}`}>
+            <div className="contact-info">
+              <p className="contact-info-name">{contact.name}</p>
+              <p className="contact-info-number">{contact.number}</p>
+            </div>
+            <div className="contact-actions">
+              <button className="btn btn-primary">Edit</button>
+              <button className="btn btn-danger">Delete</button>
+            </div>
+          </div>
+        )
+      });
 };
 
 export default ContactList;
--- src/container/Create.js
+++ src/container/Create.js
@@ -2,7 +2,27 @@ import React from "react";
 
 const Create = () => {
   return (
-    <>Contact Create</>
+    <div className="contact-form">
+      <h3>Contact Form</h3>
+      <form>
+        <div className="form-group">
+          <label className="control-label">Name</label>
+          <input type="text" className="control-input"/>
+        </div>
+        <div className="form-group">
+          <label className="control-label">Phone Number</label>
+          <input type="text" className="control-input"/>
+        </div>
+        <div className="form-group">
+          <label className="control-label">Description</label>
+          <textarea type="textarea" className="control-input"/>
+        </div>
+        <div style={{ marginTop: 20 }} className="btn-group">
+          <button type="button" className="btn btn-primary">Cancel</button>
+          <button type="submit" className="btn btn-danger">Submit</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
